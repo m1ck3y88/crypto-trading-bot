7dc52f90225f40a53418cbc716e1419948b2c70a
Michael <mickeymakesapps88@gmail.com>
2021-11-12 11:54:51 -0800

    create authenticated client object to access account portfolio for trading

--- coinbasepro_products.py
+++ coinbasepro_products.py
@@ -1,20 +1,34 @@
 import logging
-import requests
+import cbpro
+from api_key import sandbox_api_key, sandbox_api_secret, sandbox_api_pass
 
 
 logger = logging.getLogger()
 
+auth_client = cbpro.AuthenticatedClient(sandbox_api_key, sandbox_api_secret, sandbox_api_pass,
+                                        api_url="https://api-public.sandbox.pro.coinbase.com")
 
 
-def get_products():
+# print(auth_client.get_accounts())
+currency_id = 'dec357c4-0b6d-4c84-90a2-9b5df1d21613'  # USD
 
-   response_object = requests.get("https://api-public.sandbox.exchange.coinbase.com/products")
+def get_auth_products():
 
-   product_ids = []
+    product_ids = []
 
-   for product in response_object.json():
-      product_ids.append(product['id'])
+    for product in auth_client.get_products():
 
-   return product_ids
+        product_ids.append(product['id'])
 
-print(get_products())
+    return product_ids
+
+
+print(get_auth_products())  # ['BTC-GBP', 'LINK-USDC', 'ETH-BTC', 'BAT-USDC', 'BTC-EUR',
+                            # 'LINK-USD', 'BTC-USD', 'XRP-BTC', 'XRP-USD', 'XRP-GBP', 'XRP-EUR']
+
+
+
+
+
+
+
