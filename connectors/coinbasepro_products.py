5bd6863d0748b4dd7a6aabc5ef3e572f60ff7109
Michael <mickeymakesapps88@gmail.com>
2021-11-29 14:24:57 -0800

    complete algorithm for day trading on Coinbase Pro

--- connectors/coinbasepro_products.py
+++ connectors/coinbasepro_products.py
@@ -14,58 +14,39 @@ from env.api_key import *
 
 # hv.extension('bokeh')
 
-# from clients import CoinbaseProWebsocketClient
+from clients import CoinbaseProWebsocketClient
 
 logger = logging.getLogger()
 
 auth_sandbox_client = cbpro.AuthenticatedClient(sandbox_api_key, sandbox_api_secret, sandbox_api_pass,
-                                                api_url="https://api-public.sandbox.pro.coinbase.com")
+                                                api_url="https://api-public.sandbox.exchange.coinbase.com")
 
 auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
 
 
 def get_sandbox_products():
-    product_ids = []
 
     for product in auth_sandbox_client.get_products():
-        product_ids.append(product['id'])
-
-    return product_ids
+        yield product['id']
 
 
 def get_sandbox_accounts():
-    accounts = []
 
     for account in auth_sandbox_client.get_accounts():
-        accounts.append(account)
-
-    return accounts
+        yield account
 
 
 def get_live_products():
-    product_ids = []
 
     for product in auth_client.get_products():
-        product_ids.append(product['id'])
-
-    return product_ids
+        yield product['id']
 
 
 def get_live_accounts():
-    accounts = []
 
     for account in auth_client.get_accounts():
-        accounts.append(account)
+        yield account
 
-    return accounts
-
-
-# for product in get_live_products():
-#     print(product)
-
-# for account in get_live_accounts():
-#     if account['currency'] == 'JASMY':
-#         print(account)
 
 # def compute_rsi(emitted_vals):
 #     # assumes emitted_vals is a list of pandas Dataframes
@@ -148,4 +129,3 @@ def get_live_accounts():
 #
 # rtlink = RangeToolLink(rsi_chart, price_chart)
 # combined_chart = ((price_chart*mean_chart) + rsi_chart).opts(shared_axes=True, merge_tools=False).cols(1)
-
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # import tkinter as tk
-import logging
+import cbpro
 
 from connectors.coinbasepro_products import *
-
+import time
 
 logger = logging.getLogger()
 
@@ -22,20 +22,70 @@ logger.addHandler(file_handler)
 
 if __name__ == '__main__':
 
-    sandbox_products = get_sandbox_products()
-    sandbox_wallets = get_sandbox_accounts()
-    live_wallets = get_live_accounts()
+    # Live Account Algorithm
+    cur = 'USD'
+    buy_price = float(input("Please enter the buy price: "))
+    sell_price = float(input("Please enter the sell price: "))
+    coin = input("Please enter the coin abbreviation for the coin\n"
+                 "you are investing in\n"
+                 "Example: BTC\n").upper()
+    asset_id = input("Please enter the asset you want to invest in\n"
+                     "using the following format: asset-currency\n"
+                     "Example: BTC-USD\n").upper()
 
-    # for product in sandbox_products:
-    #     print(product)
+    while True:
+        price = float(auth_client.get_product_ticker(product_id=asset_id)['price'])
+        if price <= buy_price:
+            for wallet in get_live_accounts():
+                if wallet['currency'] == cur and (float(wallet['balance']) >= 5):
+                    print('Buying ' + coin + '!')
+                    print(cur + ' Available: ' + wallet['balance'])
+                    fiat_cur = '{:.2}'.format(wallet['balance'])
+                    auth_client.place_market_order(product_id=asset_id, side='buy', funds=fiat_cur)
+
+        elif price >= sell_price or price >= (buy_price * 1.025):
+            for asset in get_live_accounts():
+                if asset['currency'] == coin and (float(asset['balance']) >= 1):
+                    print('Selling ' + coin + '!')
+                    print(coin + ' Available: ' + asset['balance'])
+                    coin_cur = asset
+                    auth_client.place_market_order(product_id=asset_id, side='sell', funds=asset['balance'])
+
+        else:
+            print('Nothing yet...')
+        time.sleep(5)
+
+    # Sandbox Account Algorithm:
+    # sb_cur = 'USD'
+    # sb_buy_price = float(input("Please enter the buy price: "))
+    # sb_sell_price = float(input("Please enter the sell price: "))
+    # sb_coin = input("Please enter the coin abbreviation for the coin\n"
+    #                 "you are investing in\n"
+    #                 "Example: BTC\n").upper()
+    # sb_asset_id = input("Please enter the asset you want to invest in\n"
+    #                     "using the following format: asset-currency\n"
+    #                     "Example: BTC-USD\n").upper()
     #
-    # for wallet in sandbox_wallets:
-    #     print(wallet)
-
-    # for wallet in live_wallets:
-    #     if wallet['currency'] == 'JASMY':
-    #         jasmy_wallet = wallet
-    #         print(jasmy_wallet)
+    # while True:
+    #     sb_price = float(auth_sandbox_client.get_product_ticker(product_id=sb_asset_id)['price'])
+    #     if sb_price <= sb_buy_price:
+    #         for sb_wallet in get_sandbox_accounts():
+    #             if sb_wallet['currency'] == sb_cur and (float(sb_wallet['balance']) >= 10):
+    #                 print('Buying ' + sb_coin + '!')
+    #                 print(sb_cur + ' Available: ' + sb_wallet['balance'])
+    #                 sb_fiat_cur = '{:.2}'.format(sb_wallet['balance'])
+    #                 auth_client.place_market_order(product_id=sb_asset_id, side='buy', funds=sb_fiat_cur)
+    #     elif sb_price >= sb_sell_price or sb_price <= (sb_buy_price * 1.01):
+    #         for sb_asset in get_sandbox_accounts():
+    #             if sb_asset['currency'] == sb_coin and (float(sb_asset['balance']) >= 5):
+    #                 print('Selling ' + sb_coin + '!')
+    #                 print(sb_coin + ' Available: ' + sb_asset['balance'])
+    #                 sb_coin_cur = '{:.2}'.format(sb_asset['balance'])
+    #                 auth_sandbox_client.place_market_order(product_id=sb_asset_id, side='sell', funds=sb_coin_cur)
+    #
+    #     else:
+    #         print('Nothing yet...')
+    #     time.sleep(5)
 
     # root = tk.Tk()
     # root.configure(bg='gray12')
